7a02896a8cfb878810fbe82af321adbc3c56eed9
Lucasbc47 <lucasbarboza1costa@gmail.com>
2023-10-11 09:36:20 -0300

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-document.getElementById("bt").addEventListener("click", displayDate);
+var i = 0;
 
-function displayDate() {
-  document.getElementById("r").innerHTML = new Date();
-  
+document.getElementById("press").addEventListener("click", updateClicks);
+document.getElementById("reset").addEventListener("click", resetarClicks);
+
+function updateClicks() {
+  i++;
+  document.getElementById("times").innerHTML = `Vezes clicado: ${i}`
+}
+
+function resetarClicks() {
+  i = 0;
+  document.getElementById("times").innerHTML = `Vezes clicado:`
 }
